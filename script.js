0444aa53e0f80b54ee6a29b10b154ff34f2fc588
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-05 00:10:44 +0000

    WIP: onclick function

--- script.js
+++ script.js
@@ -1,83 +1,72 @@
 /*global $ navigator ipKey WuApi owApi*/
-
 $(document).ready(function() {
-			var lon;
-			var lat;
-			var currentPosition;
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(function(position) {
-					currentPosition = position;
-					lat = position.coords.latitude;
-					lon =position.coords.longitude;
-					$("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude)
-					// console.log(currentPosition);
-					loadWeatherData();
-				})
-			} else {
-				alert("Your location is not supported by this browser!")
-			}
-			// 	var long;
-			// 	var lat;
-			// 	$.ajax({
-			// 		method: "GET",
-			// 		url: "https://ipfind.co/?ip=8.8.8.8",
-			// 		data: {
-			// 			auth: ipKey
-			// 		},
-			// 		success: function(data) {
-			// 			console.log(data)
-			// 			for (var i = 0; i < data.length; i++) {
-			// 				var location = document.createElement("H4");
-			// 				location.setAttribute("value", data[i])
-			// 				location.innerHTML = data.latitude + " " + data.longitude;
-			// 				document.getElementById('location').appendChild(location);
-			// 				console.log("location added");
-			//  		}
-			// var api = 'https://api.wunderground.com/api//geolookup/conditions/lat=' + lat + '&lon=' + long + '&apiid'= WuApi;
-				function loadWeatherData() {
-						$.ajax({
-							method: "GET",
-							url: 'https://api.openweathermap.org/data/2.5/weather',
-							data: {
-			    				    lon: lon,
-			    				    lat: lat,
-			    				    APPID: owApi
-							},
-		
-						success: function(data2) {
-							console.log(data2);
-					var fTemp;
-					var cTemp;
-					var tempSwap = true;
-					var weatherType = data2.weather[0].description;
-					var kelvin = data2.main.temp;
-					var windSpeed = data2.wind.speed;
-					var city = data2.name;
-					var icon = data2.weather[0].icon;
-					fTemp = (kelvin * (9 / 5) - 459.67).toFixed(1); //convert kelvin to farhenheit
-					cTemp = (kelvin - 273).toFixed(1);
-					console.log(fTemp);
-					$("#city").html(city);
-					$("#weatherType").html(weatherType);
-					$("#fTemp").html(fTemp + " &#8457;");
-					$("#icon").html("<img src=https://openweathermap.org/img/w/" + icon + ".png>");
-					$("#fTemp").click(function() {
-						if (tempSwap === false) {
-							$("#fTemp").html(cTemp + " &#8451;");
-							tempSwap = true;
-						} else {
-							$("fTemp").html(fTemp + " &#8457;");
-							tempSwap = false;
-						}
-					});
-					windSpeed = (2.237 * (windSpeed)).toFixed(1);
-					$("#windSpeed").html(windSpeed + "mph");
-					// if(fTemp<50){
-					//     $('body').css('background-image', 'url    ');
-					// } else if(fTemp>70){
-					// }
-				}
-				})
-			}
-});
+	var lon;
+	var lat;
+	var currentPosition;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			currentPosition = position;
+			lat = position.coords.latitude;
+			lon = position.coords.longitude;
+			$("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude)
+			// console.log(currentPosition);
+			loadWeatherData();
+		})
+	} else {
+		alert("Your location is not supported by this browser!")
+	}
 
+	function loadWeatherData() {
+		$.ajax({
+			method: "GET",
+			url: 'https://api.openweathermap.org/data/2.5/weather',
+			data: {
+				lon: lon,
+				lat: lat,
+				APPID: owApi
+			},
+			success: function(data2) {
+				console.log(data2);
+				var fTemp;
+				var cTemp;
+				var tempSwap = true;
+				var weatherType = data2.weather[0].description;
+				var kelvin = data2.main.temp;
+				var windSpeed = data2.wind.speed;
+				var city = data2.name;
+				var icon = data2.weather[0].icon;
+				fTemp = (kelvin * (9 / 5) - 459.67).toFixed(1); //convert kelvin to farhenheit
+				cTemp = (kelvin - 273).toFixed(1);
+				windSpeed = (2.237 * (windSpeed)).toFixed(1);
+				
+				$("#city").html(city);
+				$("#weatherType").html(weatherType);
+				$("#fTemp").html(fTemp + " &#8457;");
+				$("#icon").html("<img src=https://openweathermap.org/img/w/" + icon + ".png>");
+				$("#windSpeed").html(windSpeed + "mph");
+			}
+		})
+	}				
+				
+				$('#fTemp').on('click', function(event) {
+					event.preventDefault(); // To prevent following the link (optional)
+					if(fTemp == fTemp)
+				});
+
+				$("#fTemp").click(function() {
+					if (!tempSwap) {
+						$("#fTemp").html(cTemp + " &#8451;");
+						tempSwap = true;
+					} else {
+						$("fTemp").html(fTemp + " &#8457;");
+						tempSwap = false;
+					}
+				});
+				
+				
+				// if(fTemp<50){
+				//     $('body').css('background-image', 'url    ');
+				// } else if(fTemp>70){
+				// }
+
+});		
